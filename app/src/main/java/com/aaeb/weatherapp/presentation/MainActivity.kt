4cf054aeeac542199c8f768488464f6e5bf30b3a
AAEB-DE <ayman.badawy.dev@digitalseals.de>
2023-06-18 19:10:36 +0300

    - Add forecast composable component. - Add Hourly composable component. - Handle error with UI composable.

--- app/src/main/java/com/aaeb/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/aaeb/weatherapp/presentation/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.aaeb.weatherapp.presentation
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aaeb.weatherapp.presentation.ui.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,6 +34,8 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         permissionLauncher =
@@ -36,16 +49,33 @@ class MainActivity : ComponentActivity() {
             )
         )
         setContent {
+
             WeatherAppTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.DarkGray),
-                ) {
-                    WeatherCard(
-                        state = null,
-                        backgroundColor = Color.Blue,
-                    )
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.DarkGray),
+                    ) {
+                        WeatherCard(
+                            state = viewModel.state,
+                            backgroundColor = Color.Blue,
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        WeatherForecast(state = viewModel.state)
+                    }
+                    if (viewModel.state.isLoading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                    viewModel.state.error?.let {
+                        Text(
+                            text = it, color = Color.Red,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
             }
         }
